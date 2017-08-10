a8c55343edcb298f888ae75a256fb90a666f9ffa
Allison Marie <aliederman@icloud.com>
2017-08-10 11:54:48 -0400

    starting the Liri app

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'K5IK5EaYqO89nJU03fFIBL2fz',
+  consumer_secret: 'ScfxQpaD4Zf2H9Sor82dfnytd9DFL9pnK5LE3NPq6tA2wfDwOC',
+  access_token_key: '895093572076097537-8dCDlsESujuF6EOu1VZsTOSQwCeKkD6',
+  access_token_secret: 'd0MHvfhokGaMjrLhHA5akBIUa7c5VzsFpPqJaxnUAayPv',
+}
