9599dfdf61cd0dc90feb66fe898cc7f1c1b8d42e
Allison Marie <aliederman@icloud.com>
2017-08-15 11:32:05 -0400

    finished spotify function

--- liri.js
+++ liri.js
@@ -1,8 +1,16 @@
+//KEYS
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var request = require('request');
+
+
+
+//GLOBAL VARIABLES
 var second = process.argv[2];
 var third = process.argv[3];
 var fourth = process.argv[4];
-var keys = require('./keys.js');
-var Twitter = require('twitter');
 //END OF GLOBAL VARIABLES
 
 //switch (second) {
@@ -16,7 +24,7 @@ var Twitter = require('twitter');
 //}
 
 //TWITTER NPM
-//function twitterTweets() {
+function twitterTweets() {
 
     var client = new Twitter({
         consumer_key: keys.twitterKeys.consumer_key,
@@ -32,25 +40,50 @@ var Twitter = require('twitter');
             for (var i = 19; i >= 0; i--) {
                 console.log("TWEETS " + (i + 1) + ": " + tweets[i].text);
             }
-//            console.log(tweets[0].text);
+            //            console.log(tweets[0].text);
+        } else {
+            console.log(error);
         }
     });
-//}
-//SPOTIFY NPM
-function spotify(song) {
-    var spotify = require('spotify');
-    if (third) {
-        spotify.search({
-            type: 'track',
-            query: 'song'
-        }, function (err, data) {
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-            } else {
-                console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name);
-                console.log("Album: ")
-            }
-        });
-    }
+    return;
 }
+
+if (command === "my-tweets") {
+    myTweets();
+}
+
+//SPOTIFY NPM
+function spotifyMusic(song) {
+
+    //    var spotify = new Spotify({
+    //        id: '7ceaff1dc7254746bbcdda036c96e3b1',
+    //        secret: '27185375e8f04931b988fc2b9180222a'
+    //    });
+
+    spotify.search({
+        type: 'track',
+        query: 'value',
+        limit: '1'
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else if (!err) {
+            console.log("\nArtist: " + JSON.stringify(data.tracks.items[0].name, null, 2) + "\n ");
+            console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name) + "\n ");
+            console.log("Album: " + JSON.strinigfy(data.tracks.items[0].album.name) + "\n ");
+            console.log("Link: " + JSON.stringify(data.tracks.items[0].album.external_urls));
+        }
+
+        console.log(data);
+    });
+}
+
+if (command === "spotify-song") {
+    mySpotify();
+}
+
+var queryURL = 
+
+
+
+
